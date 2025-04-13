01a5432d90bb085dcf998c1b59b378e9fc8b85e1
sktasnim2005 <sktasnim2005@gmail.com>
2025-04-14 04:13:07 +0600

    message

--- Games/Hangman_GAME(catagory)/Word_list.py
+++ Games/Hangman_GAME(catagory)/Word_list.py
@@ -2,7 +2,10 @@
 
 
 words_by_category = {
-    "Animals": ["aardvark", "alligator", "zebra", "koala", "kangaroo", "lion", "tiger", "elephant"],
-    "Fruits": ["apple", "orange", "banana", "mango", "pineapple", "grape", "watermelon", "kiwi"],
-    "Foods": ["pizza", "burger", "noodles", "biryani", "sushi", "taco", "pasta", "steak"]
+    "Animals": ["cat", "dog", "zebra", "panda", "kangaroo", "lion", "tiger", "elephant"],
+    "Fruits": ["apple", "orange", "banana", "mango", "pineapple", "grape", "watermelon", "jackfruit"],
+    "Foods": ["pizza", "burger", "noodles", "biryani", "pasta", "salad"],
+    "Countries": ["bangladesh", "india", "canada", "brazil", "australia", "france", "germany"],
+    "Colors": ["red", "blue", "green", "yellow", "purple", "orange"],
+    "Sports": ["football", "cricket", "basketball"]
 }
