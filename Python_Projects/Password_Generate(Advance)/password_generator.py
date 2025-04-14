724938ce9e6124a01c380e0b005724cbc9910409
sktasnim2005 <sktasnim2005@gmail.com>
2025-04-14 20:13:41 +0600

    message

--- Python_Projects/Password_Generate(Advance)/password_generator.py
+++ Python_Projects/Password_Generate(Advance)/password_generator.py
@@ -1,13 +1,34 @@
-import string
 import random
+import string
 
-def generate_password(length, use_numbers=True, use_special=True):
-    chars = string.ascii_letters
-    if use_numbers:
-        chars += string.digits
-    if use_special:
-        chars += "!@#$%^&*()-_+="
-    return ''.join(random.choice(chars) for _ in range(length))
+def generate_password(length, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    #special = string.punctuation
+    special = "!@#$%^&*()"
+
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+
+    if not characters:
+        return "Choose at least one character type!"
+
+    password = []
+    if numbers:
+        password.append(random.choice(digits))
+    if special_characters:
+        password.append(random.choice(special))
+    password.append(random.choice(letters))
+
+    while len(password) < length:
+        password.append(random.choice(characters))
+
+    random.shuffle(password)
+    return ''.join(password)
 
 def get_strength(password):
     length = len(password)
