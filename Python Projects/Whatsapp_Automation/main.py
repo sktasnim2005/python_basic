a7389732974dfa797b3ac339d0cacdd060919002
sktasnim2005 <sktasnim2005@gmail.com>
2025-04-14 15:24:44 +0600

    message

---
+++ Python Projects/Whatsapp_Automation/main.py
@@ -0,0 +1,7 @@
+import pywhatkit
+
+# This is a simple script to send WhatsApp messages using pywhatkit
+
+#pywhatkit.sendwhatmsg("+8801828285259", "Hello, this is a test message!", 13, 36) #tasnim
+#pywhatkit.sendwhatmsg("+8801601942069", "Hello,brother this is a test message!", 13, 36) # sahil
+#pywhatkit.sendwhatmsg("+8801965588253", "Hello,brother this is a test message!", 13, 42) # Akash
